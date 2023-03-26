21268243638c39842b2a14fbafd8f388d29f31bf
Ahmad Raza <50910798+razaahmad333@users.noreply.github.com>
2023-03-26 13:48:01 +0530

    refactored

---
+++ src/Topics/UseNCreateContext/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { ThemeProvider } from "./ThemeContext";
+import ThemeToggler from "./ThemeToggler";
+
+import WindowOne from "./Window1";
+import WindowTwo from "./Window2";
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <div className="App">
+        <ThemeToggler />
+        <WindowOne />
+        <WindowTwo />
+      </div>
+    </ThemeProvider>
+  );
+}
